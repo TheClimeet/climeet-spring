7168ba232005da7c3ed258e77daf11585af58081
gourderased <117848386+gourderased@users.noreply.github.com>
2023-12-31 18:18:45 +0900

    [SPR-13] Docs: git ignore 작성

---
+++ src/main/java/com/climeet/climeet_backend/ClimeetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.climeet.climeet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimeetBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimeetBackendApplication.class, args);
+    }
+
+}
