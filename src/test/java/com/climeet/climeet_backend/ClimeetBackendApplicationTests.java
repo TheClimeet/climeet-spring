1b53912037eb5c8ab744ce27ce8078bf5ab6630a
gourderased <117848386+gourderased@users.noreply.github.com>
2023-12-31 18:18:45 +0900

    [SPR-13] feat: git ignore 작성

---
+++ src/test/java/com/climeet/climeet_backend/ClimeetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climeet.climeet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimeetBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
