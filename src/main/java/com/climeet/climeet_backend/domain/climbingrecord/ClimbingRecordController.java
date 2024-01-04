b62cd34e25380adb925aad550636e22e16babbc4
tjdgns8439 <tjdgns8439@inha.edu>
2024-01-04 23:42:53 +0900

    [SPR-31] feat: 조회 관련 Controller 추가
    - 간편 기록 조회
    - 기록 날짜로 조회
    - 기록 id로 조회

--- src/main/java/com/climeet/climeet_backend/domain/climbingrecord/ClimbingRecordController.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbingrecord/ClimbingRecordController.java
@@ -1,13 +1,20 @@
 package com.climeet.climeet_backend.domain.climbingrecord;
 
 import com.climeet.climeet_backend.domain.climbingrecord.dto.ClimbingRecordRequestDto;
+import com.climeet.climeet_backend.domain.climbingrecord.dto.ClimbingRecordResponseDto;
+import com.climeet.climeet_backend.domain.climbingrecord.dto.ClimbingRecordResponseDto.ClimbingRecordSimpleInfo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.joda.time.DateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -22,10 +29,48 @@ public class ClimbingRecordController {
 
     private final ClimbingRecordService climbingRecordService;
 
-    @Operation(summary = "클라이밍기록생성", description = "클라이밍 기록 생성")
+    /**
+     * @param "ClimbingRecordRequestDto requestDto"
+     */
+    @Operation(summary = "클라이밍 기록 생성", description = "클라이밍 기록 생성")
     @PostMapping
     public ResponseEntity<?> addClimbingRecord(@RequestBody ClimbingRecordRequestDto requestDto) {
         climbingRecordService.createClimbingRecord(requestDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    /**
+     * 간편기록 총 조회
+     *
+     * @param
+     * @return List<ClimbingRecordResponseDto>
+     */
+    @Operation(summary = "클라이밍 간편 기록 조회", description = "클라이밍 기록 생성")
+    @GetMapping
+    public ResponseEntity<List<ClimbingRecordSimpleInfo>> getClimbingRecordList() {
+        return new ResponseEntity<>(climbingRecordService.getClimbingRecordSimpleInfoList(),
+            HttpStatus.OK);
+    }
+
+    /**
+     * @param "date"
+     * @return ClimingRecordResponseDto
+     */
+    @Operation(summary = "클라이밍 기록 조회", description = "클라이밍 기록 생성")
+    @GetMapping("/{date}")
+    public ResponseEntity<List<ClimbingRecordSimpleInfo>> addClimbingRecord(
+        @PathVariable LocalDate date) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * @param "id"
+     * @return ClimingRecordResponseDto
+     */
+    @Operation(summary = "클라이밍 기록 조회", description = "클라이밍 기록 생성")
+    @GetMapping("/{id}")
+    public ResponseEntity<ClimbingRecordSimpleInfo> addClimbingRecord(@PathVariable Long id) {
+        return new ResponseEntity<>(climbingRecordService.getClimbingRecord(id), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/climeet/climeet_backend/domain/climbingrecord/ClimbingRecordService.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbingrecord/ClimbingRecordService.java
@@ -3,20 +3,16 @@ package com.climeet.climeet_backend.domain.climbingrecord;
 import com.climeet.climeet_backend.domain.climbinggym.ClimbingGym;
 import com.climeet.climeet_backend.domain.climbinggym.ClimbingGymRepository;
 import com.climeet.climeet_backend.domain.climbingrecord.dto.ClimbingRecordRequestDto;
-import com.climeet.climeet_backend.domain.climbingrecord.dto.ClimbingRecordResponseDto;
-import com.climeet.climeet_backend.domain.route.RouteRepository;
-import com.climeet.climeet_backend.domain.routerecord.RouteRecord;
-import com.climeet.climeet_backend.domain.routerecord.RouteRecordRepository;
+import com.climeet.climeet_backend.domain.climbingrecord.dto.ClimbingRecordResponseDto.ClimbingRecordSimpleInfo;
+import com.climeet.climeet_backend.domain.routerecord.RouteRecordService;
 import com.climeet.climeet_backend.global.response.ApiResponse;
 import com.climeet.climeet_backend.global.response.code.status.ErrorStatus;
 import com.climeet.climeet_backend.global.response.exception.GeneralException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
+import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static com.climeet.climeet_backend.domain.routerecord.RouteRecord.toEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
@@ -24,9 +20,10 @@ public class ClimbingRecordService {
 
     private final ClimbingRecordRepository climbingRecordRepository;
     private final ClimbingGymRepository gymRepository;
-    private final RouteRecordRepository routeRecordRepository;
-    private final RouteRepository routeRepository;
+    private final RouteRecordService routeRecordService;
 
+
+    @Transactional
     public ApiResponse<String> createClimbingRecord(ClimbingRecordRequestDto requestDto) {
         try {
             ClimbingGym climbingGym = gymRepository.findByName(requestDto.getClimbingGymName());
@@ -35,15 +32,28 @@ public class ClimbingRecordService {
                 .save(ClimbingRecord.toEntity(requestDto, climbingGym));
 
             // 루트기록 리퀘스트 돌면서 루트 리퀘스트 저장
-            List<RouteRecord> routeRecords = requestDto.getRouteRecordRequestDtoList().stream()
-                .map(dto -> toEntity(dto, climbingRecord,
-                    routeRepository.findById(dto.getRouteId()).get()))
-                .collect(Collectors.toList());
-            routeRecordRepository.saveAll(routeRecords);
+            requestDto.getRouteRecordRequestDtoList().stream()
+                .map(dto -> routeRecordService.addRouteRecord(dto, climbingRecord));
+
             return ApiResponse.onSuccess("클라이밍 기록생성 성공");
         } catch (Exception e) {
             throw new GeneralException(ErrorStatus._BAD_REQUEST);
         }
+    }
 
+    public List<ClimbingRecordSimpleInfo> getClimbingRecordSimpleInfoList() {
+        try {
+            List<ClimbingRecord> recordList = climbingRecordRepository.findAll();
+            return recordList.stream()
+                .map(ClimbingRecordSimpleInfo::new)
+                .toList();
+        } catch (Exception e) {
+            throw new GeneralException(ErrorStatus._BAD_REQUEST);
+        }
+    }
+
+    // TODO: 2024/01/04 운동기록 id로 조회(하나) & date로 찾기(여러 개)
+    public ClimbingRecordSimpleInfo getClimbingRecord(Long id) {
+        return new ClimbingRecordSimpleInfo();
     }
 }
