e9492233f5bfa8aabc2f6da49e0b38c6bc63cf18
tjdgns8439 <tjdgns8439@inha.edu>
2024-01-04 23:34:58 +0900

    [SPR-31] fix: 타입 변경
    - 클라이밍기록에 저장될 운동 일자와 운동 시간을 String에서 LocatDate/Time으로 변경하였습니다.

--- src/main/java/com/climeet/climeet_backend/domain/climbingrecord/dto/ClimbingRecordRequestDto.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbingrecord/dto/ClimbingRecordRequestDto.java
@@ -2,6 +2,8 @@ package com.climeet.climeet_backend.domain.climbingrecord.dto;
 
 import com.climeet.climeet_backend.domain.routerecord.dto.RouteRecordRequestDto;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,14 +18,15 @@ import java.util.List;
 public class ClimbingRecordRequestDto {
 
     @Schema(example = "2024-01-04", description = "날짜입력")
-    private String date;
+    private LocalDate date;
 
-    @Schema(example = "120", description = "분단위 시간")
-    private int time;
+    @Schema(example = "1-0-0", description = "시간")
+    private LocalTime time;
 
     @Schema(example = "더클라임 홍대점", description = "클라이밍 지점 이름")
     private String climbingGymName;
 
     @Schema(description = "클라이밍 루트 목록")
     private List<RouteRecordRequestDto> routeRecordRequestDtoList;
+
 }
