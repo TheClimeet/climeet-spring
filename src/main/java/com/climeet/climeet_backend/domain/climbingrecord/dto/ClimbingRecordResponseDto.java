62a83645769d852f9884af67113ac4215d09289d
tjdgns8439 <tjdgns8439@inha.edu>
2024-01-04 23:31:52 +0900

    [SPR-31] feat: ClimbingRecordSimpleInfo 생성
    - 캘린더 클릭 시 반환하는 정보

--- src/main/java/com/climeet/climeet_backend/domain/climbingrecord/dto/ClimbingRecordResponseDto.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbingrecord/dto/ClimbingRecordResponseDto.java
@@ -1,5 +1,35 @@
 package com.climeet.climeet_backend.domain.climbingrecord.dto;
 
-public class ClimbingRecordResponseDto {
+import com.climeet.climeet_backend.domain.climbinggym.ClimbingGym;
+import com.climeet.climeet_backend.domain.climbingrecord.ClimbingRecord;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+public class ClimbingRecordResponseDto {
+    @Getter
+    @NoArgsConstructor
+    public static class ClimbingRecordSimpleInfo{
+
+        private LocalDate date;
+        private String gymName;
+        private String gymProfileImg;
+        private LocalTime time;
+        private Integer totalCompletedCount;
+        private Integer totalAttemptCount;
+        private Integer avgDifficulty;
+
+        public ClimbingRecordSimpleInfo(ClimbingRecord climbingRecord) {
+            this.date = climbingRecord.getClimbingDate();
+            this.gymName = climbingRecord.getGym().getName();
+            this.avgDifficulty = climbingRecord.getAvgDifficulty();
+            this.time = climbingRecord.getClimbingTime();
+            this.gymProfileImg = climbingRecord.getGym().getProfileImageUrl();
+            this.totalAttemptCount = climbingRecord.getTotalAttemptCount();
+            this.totalCompletedCount = climbingRecord.getTotalCompletedCount();
+        }
+    }
 }
