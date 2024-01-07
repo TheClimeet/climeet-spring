2de6220754e9182d7587651af1a6a130a2ad0faf
Ga Dong Sik <ret0422@gmail.com>
2024-01-07 22:15:02 +0900

    [SPR-45] feat: 루트 생성 기능 구현
    To-do
    - 예외 처리 추가 예정
    - 입력된 gymId 값과 더블 체크 예정

--- src/main/java/com/climeet/climeet_backend/domain/route/RouteController.java
+++ src/main/java/com/climeet/climeet_backend/domain/route/RouteController.java
@@ -1,5 +1,30 @@
 package com.climeet.climeet_backend.domain.route;
 
+import com.climeet.climeet_backend.domain.route.dto.RouteRequestDto.RouteCreateRequestDto;
+import com.climeet.climeet_backend.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
 public class RouteController {
 
+    private final RouteService routeService;
+
+    // 암장 루트 추가
+    @PostMapping("/gym/{gymId}/route")
+    public ApiResponse<String> createRoute(@PathVariable Long gymId,
+        @RequestBody RouteCreateRequestDto routeCreateRequestDto) {
+        routeService.createRoute(
+            gymId,
+            routeCreateRequestDto.getSectorId(),
+            routeCreateRequestDto.getName(),
+            routeCreateRequestDto.getDifficulty(),
+            routeCreateRequestDto.getRouteImageUrl());
+        return ApiResponse.onSuccess("새로운 Route를 추가했습니다.");
+    }
 }
--- src/main/java/com/climeet/climeet_backend/domain/route/RouteService.java
+++ src/main/java/com/climeet/climeet_backend/domain/route/RouteService.java
@@ -1,10 +1,30 @@
 package com.climeet.climeet_backend.domain.route;
 
+import com.climeet.climeet_backend.domain.sector.Sector;
+import com.climeet.climeet_backend.domain.sector.SectorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class RouteService {
+
+    private final RouteRepository routeRepository;
+    private final SectorRepository sectorRepository;
+
+    @Transactional
+    public void createRoute(Long gymId, Long sectorId, String name, int difficulty,
+        String routeImageUrl) {
+        Sector sector = sectorRepository.findById(sectorId).orElseThrow();
+        Route route = Route.builder()
+            .sector(sector)
+            .name(name)
+            .difficulty(difficulty)
+            .routeImageUrl(routeImageUrl)
+            .build();
+
+        routeRepository.save(route);
+    }
 
 }
