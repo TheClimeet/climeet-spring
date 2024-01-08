62a814f6cbfcd486706d4811ef6b9cecce7bab04
gourderased <gourderased@gmail.com>
2024-01-09 01:30:16 +0900

    [SPR-41] docs: 숏츠 업로드 스웨거 설정 추가

--- src/main/java/com/climeet/climeet_backend/domain/shorts/ShortsController.java
+++ src/main/java/com/climeet/climeet_backend/domain/shorts/ShortsController.java
@@ -2,12 +2,15 @@ package com.climeet.climeet_backend.domain.shorts;
 
 import com.climeet.climeet_backend.domain.shorts.dto.ShortsRequestDto.PostShortsReq;
 import com.climeet.climeet_backend.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "shorts", description = "숏츠 API")
 @RequiredArgsConstructor
 @RestController
 public class ShortsController {
@@ -15,6 +18,7 @@ public class ShortsController {
     private final ShortsService shortsService;
 
     @PostMapping("/shorts")
+    @Operation(summary = "숏츠 업로드")
     public ApiResponse<String> uploadShorts(@RequestPart(value = "video") MultipartFile video,
         @RequestPart MultipartFile thumbnailImage,
         @RequestPart PostShortsReq postShortsReq) {
