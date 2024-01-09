310d0ea8956a187e419d9de42ecc49520966c915
Ga Dong Sik <62535229+Nesquitto@users.noreply.github.com>
2024-01-09 17:13:18 +0900

    [SPR-40] feat: 루트 생성, 조회 구현
    * [SPR-40] feat: Sector 엔티티 수정
    
    - sectionName -> sectorName (오타수정)
    - floor 컬럼 추가 (복층 구조인 암장 존재)
    
    * [SPR-40] feat: Sector 엔티티 수정
    
    floor 컬럼이 추가 안되서 다시 추가..
    
    * [SPR-45] feat: 루트 생성 기능 구현
    
    To-do
    - 예외 처리 추가 예정
    - 입력된 gymId 값과 더블 체크 예정
    
    * [SRP-48] feat: 루트 조회 기능 구현
    
    - 루트 Id로 루트 정보 조회
    
    To-do
    - 루트 존재하지 않을 시 예외 처리 구현
    
    * [SPR-48] feat: 루트 조회 기능 구현
    
    - 루트 Id로 루트 정보 조회
    
    To-do
    - 루트 존재하지 않을 시 예외 처리 구현
    
    * [SPR-53] feat: 전체 루트 조회 기능 구현
    
    - gymId에 해당하는 암장의 전체 루트 정보를 조회
    - 루트 목록만 조회하는 기능으로 변경할지는 추후 해당 API가 어디에 쓰일지 구체적으로 체크하고 판단
    - 암장이 존재하지 않는 예외처리 예정
    - 조회할 루트가 없는 경우도 예외처리할지는 고민
    
    * [SPR-40] feat: Controller return 변경
    
    - ResponseEntity를 사용하던 것을 ApiResponse로 변경 (언제 쓰는건지 깨달음..)
    
    * [SPR-40] feat: Sector 엔티티 수정, RouteController 의존관계 정리
    
    - Sector 엔티티의 Floor 기본값을 0으로 입력
    - RouteController의 ResponseEntity 의존 삭제

--- src/main/java/com/climeet/climeet_backend/domain/route/RouteController.java
+++ src/main/java/com/climeet/climeet_backend/domain/route/RouteController.java
@@ -1,5 +1,45 @@
 package com.climeet.climeet_backend.domain.route;
 
+import com.climeet.climeet_backend.domain.route.dto.RouteRequestDto.RouteCreateRequestDto;
+import com.climeet.climeet_backend.domain.route.dto.RouteResponseDto.RouteGetResponseDto;
+import com.climeet.climeet_backend.global.response.ApiResponse;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
 public class RouteController {
 
+    private final RouteService routeService;
+
+    // 암장 루트 추가
+    @PostMapping("/gym/{gymId}/route")
+    public ApiResponse<String> createRoute(@PathVariable Long gymId,
+        @RequestBody RouteCreateRequestDto routeCreateRequestDto) {
+        routeService.createRoute(
+            gymId,
+            routeCreateRequestDto.getSectorId(),
+            routeCreateRequestDto.getName(),
+            routeCreateRequestDto.getDifficulty(),
+            routeCreateRequestDto.getRouteImageUrl());
+        return ApiResponse.onSuccess("새로운 Route를 추가했습니다.");
+    }
+
+    // 암장 루트 정보 조회
+    @GetMapping("/gym/route/{routeId}")
+    public ApiResponse<RouteGetResponseDto> findRouteByRouteId(@PathVariable Long routeId) {
+        return ApiResponse.onSuccess(routeService.getRoute(routeId));
+    }
+
+    // 암장 루트 리스트 정보 조회
+    @GetMapping("/gym/{gymId}/routes")
+    public ApiResponse<List<RouteGetResponseDto>> findAllRouteByGymId(@PathVariable Long gymId) {
+        return ApiResponse.onSuccess(routeService.getRouteList(gymId));
+    }
 }
