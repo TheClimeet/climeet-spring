ad028d180024ed8c999878bb48c569f666b91e74
tjdgns8439 <tjdgns8439@inha.edu>
2024-01-11 17:31:13 +0900

    [SPR-54] chore: findById 삭제

--- src/main/java/com/climeet/climeet_backend/domain/route/RouteRepository.java
+++ src/main/java/com/climeet/climeet_backend/domain/route/RouteRepository.java
@@ -4,6 +4,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RouteRepository extends JpaRepository<Route, Long> {
-    Optional<Route> findById(Long id);
     List<Route> findBySectorClimbingGymId(Long gymId);
 }
