301a697ed3f4e33de0a22b4d29fbace2881c72f7
Yerim Park(박예림) <85860891+yerim216@users.noreply.github.com>
2024-01-14 13:30:19 +0700

    Feature/spr 37 (#20)
    * [SPR-37] feat: 회원가입 RequestDto 정보 추가
    
    * [SPR-37] feat: swagger에 enum 정보 추가

--- src/main/java/com/climeet/climeet_backend/domain/climber/dto/ClimberSignUpRequestDto.java
+++ src/main/java/com/climeet/climeet_backend/domain/climber/dto/ClimberSignUpRequestDto.java
@@ -2,6 +2,7 @@ package com.climeet.climeet_backend.domain.climber.dto;
 
 import com.climeet.climeet_backend.domain.climber.enums.ClimbingLevel;
 import com.climeet.climeet_backend.domain.climber.enums.DiscoveryChannel;
+import java.util.List;
 import lombok.Data;
 import lombok.Getter;
 
@@ -10,9 +11,12 @@ public class ClimberSignUpRequestDto {
     private String nickName;
     private ClimbingLevel climbingLevel;
     private DiscoveryChannel discoveryChannel;
-//    private Boolean isAllowFollowNotification;
-//    private Boolean isAllowLikeNotification;
-//    private Boolean isAllowCommentNotification;
-//    private Boolean isAllowAdNotification;
+    private String profileImgUrl;
+    private List<String> gymFollowList;
+    private Boolean isAllowFollowNotification;
+    private Boolean isAllowLikeNotification;
+    private Boolean isAllowCommentNotification;
+    private Boolean isAllowAdNotification;
+
 
 }
