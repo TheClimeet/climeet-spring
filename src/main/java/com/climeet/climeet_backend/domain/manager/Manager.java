892d55e44c7e855ae59810af47c578f438ebb08f
yerim216 <bing_01@naver.com>
2024-01-16 02:07:41 +0900

    [SPR-38] feat: Manager - ClimbingGym 1대1 연관관계 추가(ClimbingGym이 Manager 컬럼 값 소유), 사업자등록증 url 컬럼 @NotNull 임시 삭제

--- src/main/java/com/climeet/climeet_backend/domain/manager/Manager.java
+++ src/main/java/com/climeet/climeet_backend/domain/manager/Manager.java
@@ -1,21 +1,36 @@
 package com.climeet.climeet_backend.domain.manager;
 
+import com.climeet.climeet_backend.domain.climbinggym.ClimbingGym;
+import com.climeet.climeet_backend.domain.climbinggym.ClimbingGymRepository;
+import com.climeet.climeet_backend.domain.climbinggym.ClimbingGymService;
+import com.climeet.climeet_backend.domain.manager.dto.ManagerRequestDto.CreateManagerRequest;
 import com.climeet.climeet_backend.domain.user.User;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.NotNull;
+import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Builder
 public class Manager extends User {
 
+    @Setter
+    @OneToOne
+    @JoinColumn(name="id")
+    private ClimbingGym climbingGym;
+
     @NotNull
-    private String LoginId;
+    private String loginId;
 
     @NotNull
     private String password;
@@ -29,8 +44,9 @@ public class Manager extends User {
     @NotNull
     private String email;
 
-    @NotNull
     private String businessRegistrationImageUrl;
 
     private Boolean isRegistered = false;
+
+
 }
