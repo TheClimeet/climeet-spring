5dcfa42450fa523042b8596befe2aa5ce077b08c
yerim216 <bing_01@naver.com>
2024-01-16 02:08:39 +0900

    [SPR-38] feat: ClimbingGymBackgroundImage 연관관계 컬럼 매핑 추가 및 ClimbingGymBackgroundImageRepository 생성

--- src/main/java/com/climeet/climeet_backend/domain/climbinggymimage/ClimbingGymBackgroundImage.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbinggymimage/ClimbingGymBackgroundImage.java
@@ -7,17 +7,21 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@Builder
 public class ClimbingGymBackgroundImage extends BaseTimeEntity {
 
     @Id
@@ -25,8 +29,11 @@ public class ClimbingGymBackgroundImage extends BaseTimeEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="climbing_gym_id")
     private ClimbingGym climbingGym;
 
     @NotNull
     private String imgUrl;
+
+
 }
