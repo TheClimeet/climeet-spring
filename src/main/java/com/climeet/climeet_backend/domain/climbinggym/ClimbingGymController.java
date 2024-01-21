172de196ced3e3f8f1cf3cbb72db06ac883e477e
Ga Dong Sik <62535229+Nesquitto@users.noreply.github.com>
2024-01-21 16:26:26 +0900

    [SPR-66] 암장 검색기능 구현
    * [SPR-66] feat: 암장 검색기능 구현
    
    - manager가 등록되어있는 암장 검색 기능
    - manager에 상관없이 등록되어있는 암장 검색 기능
    총 두가지 구현하였습니다. 암장 데이터 약 350개는 구글 시트에서 DB로 옮겨서 기본 값만 입력해 구현했습니다.
    
    * [SPR-66] feat: ResponseDto Builder 패턴으로 변경
    
    * [SPR-66] feat: findById 삭제
    
    * [SPR-66] fix: 가독성 향상을 위한 변수 이름 수정
    
    word -> gymName

--- src/main/java/com/climeet/climeet_backend/domain/climbinggym/ClimbingGymController.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbinggym/ClimbingGymController.java
@@ -1,10 +1,47 @@
 package com.climeet.climeet_backend.domain.climbinggym;
 
+import com.climeet.climeet_backend.domain.climbinggym.dto.ClimbingGymResponseDto.ClimbingGymSimpleResponse;
+import com.climeet.climeet_backend.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
+@Tag(name = "ClimbingGym", description = "암장 관련 API")
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/gym")
 public class ClimbingGymController {
+
+    private final ClimbingGymService climbingGymService;
+
+    /**
+     * [GET] 암장검색 (Manager가 등록이 되어있지 않은 암장도 조회)
+     * Manager 유무도 함께 반환
+     */
+    @Operation(summary = "전국 암장 검색 기능(자체 목록화)")
+    @GetMapping("/search/all")
+    public ResponseEntity<List<ClimbingGymSimpleResponse>> getClimbingGymSearchingList(
+        @RequestParam("gymname") String gymName
+    ) {
+        return ResponseEntity.ok(climbingGymService.searchClimbingGym(gymName));
+    }
+
+    /**
+     * [GET] 암장검색 (Manager가 등록되어있는 암장만 조회)
+     */
+    @Operation(summary = "Manager가 등록된 암장 검색 기능")
+    @GetMapping("/search")
+    public ResponseEntity<List<ClimbingGymSimpleResponse>> getAcceptedClimbingGymSearchingList(
+        @RequestParam("gymname") String gymName
+    ) {
+        return ResponseEntity.ok(climbingGymService.searchAcceptedClimbingGym(gymName));
+    }
 
 }
