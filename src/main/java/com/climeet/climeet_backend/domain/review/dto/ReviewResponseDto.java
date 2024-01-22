0e125966802118f5888ef0509effb544f9e67c3b
Ga Dong Sik <ret0422@gmail.com>
2024-01-22 14:26:31 +0900

    [SPR-80] feat: 리뷰 조회 구현
    리뷰 조회 기능 구현
    1. Summary로 해당 암장의 평균 rating, 전체 리뷰 수, 내가 작성한 리뷰 조회
    2. list로 해당 암장의 리뷰 목록을 최신순으로 조회(페이징 처리, 내가 작성한 리뷰 제외)
    3. 스웨거 예외응답 추가
    4. 기타 주석 및, 불필요 요소 제거

---
+++ src/main/java/com/climeet/climeet_backend/domain/review/dto/ReviewResponseDto.java
@@ -0,0 +1,63 @@
+package com.climeet.climeet_backend.domain.review.dto;
+
+import com.climeet.climeet_backend.domain.climbinggym.ClimbingGym;
+import com.climeet.climeet_backend.domain.review.Review;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class ReviewResponseDto {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewSummaryDetailResponse {
+
+        private Long gymId;
+        private Float averageRating;
+        private int reviewCount;
+        @Setter
+        private ReviewDetailResponse myReview;
+
+        public static ReviewSummaryDetailResponse toDTO(ClimbingGym climbingGym,
+            ReviewDetailResponse myReview) {
+            return ReviewSummaryDetailResponse.builder()
+                .gymId(climbingGym.getId())
+                .averageRating(climbingGym.getAverageRating())
+                .reviewCount(climbingGym.getReviewCount())
+                .myReview(myReview)
+                .build();
+        }
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewDetailResponse {
+
+        private Long userId;
+        private String profileImageUrl;
+        private String profileName;
+        private Float rating;
+        private String content;
+        private LocalDateTime updatedAt;
+
+        public static ReviewDetailResponse toDTO(Review review) {
+            return ReviewDetailResponse.builder()
+                .userId(review.getClimber().getId())
+                .profileImageUrl(review.getClimber().getProfileImageUrl())
+                .profileName(review.getClimber().getProfileName())
+                .rating(review.getRating())
+                .content(review.getContent())
+                .updatedAt(review.getUpdatedAt())
+                .build();
+
+        }
+    }
+
+}
