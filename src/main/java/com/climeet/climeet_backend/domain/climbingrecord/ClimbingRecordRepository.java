ba0c84abf814745b5f0c274c7ce794e1445c29ed
tjdgns8439 <100510247+tjdgns8439@users.noreply.github.com>
2024-01-22 19:59:27 +0900

    [SPR-65] 월별 통계기능 구현
    * [SPR-65] feat: 월별암장통계 구현
    
    * [SPR-65] feat: 통계기록반환
    
    - 쿼리에서 바로 dto를 만들어 반환하려던 방법을 버리고 Tuple로 받은 후 각 값에 맞게 mapping해준 후 dto로 변환하여 반환하였습니다.
    - 쿼리 후 totalTime은 Double로 반환되기 때문에 LocalTime으로 convert해주는 로직을 작성하였습니다.
    
    * [SPR-65] feat: routeRecordDate 필드 추가
    
    * [SPR-65] feat: find
    
    - 일정기간동안 기록된 루트 기록들의 Difficulty와 그에 해당하는 횟수 조회
    
    * [SPR-65] feat: 월별통계조회
    
    * [SPR-65] chore: 필요없는 import문 삭제
    
    * [SPR-65] feat: START_DAY_OF_MONTH 추가
    
    - 진로의 피드백을 받아 월 초를 매직넘버 1이 아닌 상수를 추가하여 가독성을 높였습니다.
    
    * [SPR-65] refactor: 빌더패턴 적용
    
    - responseDto를 피드백과 팀원들의 컨벤션에 맞추어 ~~.toDTO로 변경하였습니다.
    - 매직 넘버 수정하였습니다.
    
    * [SPR-65] refactor: 응답 방식 변경
    
    * [SPR-65] chore: 사용하지 않는 import문 제거

--- src/main/java/com/climeet/climeet_backend/domain/climbingrecord/ClimbingRecordRepository.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbingrecord/ClimbingRecordRepository.java
@@ -1,13 +1,26 @@
 package com.climeet.climeet_backend.domain.climbingrecord;
 
+import jakarta.persistence.Tuple;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ClimbingRecordRepository extends JpaRepository<ClimbingRecord, Long> {
 
     List<ClimbingRecord> findByClimbingDateBetween(LocalDate startDate, LocalDate endDate);
 
     Optional<ClimbingRecord> findById(Long id);
+
+    @Query("SELECT " +
+        "   SUM(HOUR(cr.climbingTime) * 3600 + MINUTE(cr.climbingTime) * 60 + SECOND(cr.climbingTime)) as totalTime, " +
+        "   SUM(cr.totalCompletedCount) as totalCompletedCount, " +
+        "   SUM(cr.attemptRouteCount) as attemptRouteCount " +
+        "FROM ClimbingRecord cr " +
+        "WHERE cr.climbingDate BETWEEN :startDate AND :endDate")
+    Tuple getStatisticsInfoBetween(@Param("startDate") LocalDate startDate,
+        @Param("endDate") LocalDate endDate);
+
 }
--- src/main/java/com/climeet/climeet_backend/domain/climbingrecord/dto/ClimbingRecordResponseDto.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbingrecord/dto/ClimbingRecordResponseDto.java
@@ -1,8 +1,14 @@
 package com.climeet.climeet_backend.domain.climbingrecord.dto;
 
 import com.climeet.climeet_backend.domain.climbingrecord.ClimbingRecord;
+import com.climeet.climeet_backend.domain.routerecord.dto.RouteRecordResponseDto.RouteRecordSimpleInfo;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,9 +16,11 @@ public class ClimbingRecordResponseDto {
 
     @Getter
     @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
     public static class ClimbingRecordSimpleInfo {
 
-        private Long ClimbingRecordId;
+        private Long climbingRecordId;
         private LocalDate date;
         private LocalTime time;
         private Integer totalCompletedCount;
@@ -20,14 +28,70 @@ public class ClimbingRecordResponseDto {
         private Integer avgDifficulty;
         private Long gymId;
 
-        public ClimbingRecordSimpleInfo(ClimbingRecord climbingRecord) {
-            this.ClimbingRecordId = climbingRecord.getId();
-            this.date = climbingRecord.getClimbingDate();
-            this.avgDifficulty = climbingRecord.getAvgDifficulty();
-            this.time = climbingRecord.getClimbingTime();
-            this.totalAttemptCount = climbingRecord.getTotalAttemptCount();
-            this.totalCompletedCount = climbingRecord.getTotalCompletedCount();
-            this.gymId = climbingRecord.getId();
+        public static ClimbingRecordSimpleInfo toDTO(ClimbingRecord climbingRecord) {
+            return ClimbingRecordSimpleInfo.builder()
+                .climbingRecordId(climbingRecord.getId())
+                .date(climbingRecord.getClimbingDate())
+                .avgDifficulty(climbingRecord.getAvgDifficulty())
+                .time(climbingRecord.getClimbingTime())
+                .totalAttemptCount(climbingRecord.getTotalAttemptCount())
+                .totalCompletedCount(climbingRecord.getTotalCompletedCount())
+                .gymId(climbingRecord.getGym().getId())
+                .build();
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class ClimbingRecordDetailInfo {
+
+        private Long climbingRecordId;
+        private LocalDate date;
+        private LocalTime time;
+        private Integer totalCompletedCount;
+        private Integer totalAttemptCount;
+        private Integer avgDifficulty;
+        private Long gymId;
+        private List<RouteRecordSimpleInfo> routeRecordSimpleInfoList;
+
+        public static ClimbingRecordDetailInfo toDTO(ClimbingRecord climbingRecord,
+            List<RouteRecordSimpleInfo> routeRecordSimpleInfoList) {
+
+            return ClimbingRecordDetailInfo.builder()
+                .climbingRecordId(climbingRecord.getId())
+                .date(climbingRecord.getClimbingDate())
+                .avgDifficulty(climbingRecord.getAvgDifficulty())
+                .time(climbingRecord.getClimbingTime())
+                .totalAttemptCount(climbingRecord.getTotalAttemptCount())
+                .totalCompletedCount(climbingRecord.getTotalCompletedCount())
+                .gymId(climbingRecord.getGym().getId())
+                .routeRecordSimpleInfoList(routeRecordSimpleInfoList)
+                .build();
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class ClimbingRecordStatisticsInfo {
+
+        private LocalTime time;
+        private Long totalCompletedCount;
+        private Long attemptRouteCount;
+        private List<Map<Long, Long>> difficulty;
+
+        public static ClimbingRecordStatisticsInfo toDTO(LocalTime time, Long totalCompletedCount,
+            Long attemptRouteCount, List<Map<Long, Long>> difficulty) {
+
+            return ClimbingRecordStatisticsInfo.builder()
+                .time(time)
+                .totalCompletedCount(totalCompletedCount)
+                .attemptRouteCount(attemptRouteCount)
+                .difficulty(difficulty)
+                .build();
         }
     }
 }
