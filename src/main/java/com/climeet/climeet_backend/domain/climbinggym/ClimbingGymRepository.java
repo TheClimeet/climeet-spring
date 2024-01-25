a49f1d74d6257dfceb5599fa6504c448a857faab
Ga Dong Sik <ret0422@gmail.com>
2024-01-25 11:48:09 +0900

    [SPR-66] feat: Pagenation 구현
    암장 검색에서 page, size로 검색 가능하게 구현

--- src/main/java/com/climeet/climeet_backend/domain/climbinggym/ClimbingGymRepository.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbinggym/ClimbingGymRepository.java
@@ -2,6 +2,7 @@ package com.climeet.climeet_backend.domain.climbinggym;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ public interface ClimbingGymRepository extends JpaRepository<ClimbingGym, Long> 
 
     Optional<ClimbingGym> findByName(String gymName);
 
-    List<ClimbingGym> findByNameContaining(String gymName, Pageable pageable);
+    Page<ClimbingGym> findByNameContaining(String gymName, Pageable pageable);
 
-    List<ClimbingGym> findByNameContainingAndManagerIsNotNull(String gymName, Pageable pageable);
+    Page<ClimbingGym> findByNameContainingAndManagerIsNotNull(String gymName, Pageable pageable);
 }
--- src/main/java/com/climeet/climeet_backend/domain/climbinggym/ClimbingGymService.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbinggym/ClimbingGymService.java
@@ -2,10 +2,12 @@ package com.climeet.climeet_backend.domain.climbinggym;
 
 import com.climeet.climeet_backend.domain.climbinggym.dto.ClimbingGymResponseDto.AcceptedClimbingGymSimpleResponse;
 import com.climeet.climeet_backend.domain.climbinggym.dto.ClimbingGymResponseDto.ClimbingGymSimpleResponse;
+import com.climeet.climeet_backend.global.common.PageResponseDto;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -14,25 +16,33 @@ public class ClimbingGymService {
 
     private final ClimbingGymRepository climbingGymRepository;
 
-    public List<ClimbingGymSimpleResponse> searchClimbingGym(String gymName) {
-        Pageable pageable = PageRequest.of(0, 20);
-        List<ClimbingGym> climbingGymList = climbingGymRepository.findByNameContaining(gymName, pageable);
+    public PageResponseDto<List<ClimbingGymSimpleResponse>> searchClimbingGym(String gymName,
+        int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Slice<ClimbingGym> climbingGymSlice = climbingGymRepository.findByNameContaining(gymName,
+            pageable);
 
-        return climbingGymList.stream()
+        List<ClimbingGymSimpleResponse> climbingGymList = climbingGymSlice.stream()
             .map(climbingGym -> ClimbingGymSimpleResponse.toDTO(climbingGym))
             .toList();
 
+        return new PageResponseDto<>(pageable.getPageNumber(), climbingGymSlice.hasNext(),
+            climbingGymList);
+
     }
 
-    public List<AcceptedClimbingGymSimpleResponse> searchAcceptedClimbingGym(String gymName) {
-        Pageable pageable = PageRequest.of(0, 20);
-        List<ClimbingGym> climbingGymList = climbingGymRepository.findByNameContainingAndManagerIsNotNull(
+    public PageResponseDto<List<AcceptedClimbingGymSimpleResponse>> searchAcceptedClimbingGym(
+        String gymName, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Slice<ClimbingGym> climbingGymSlice = climbingGymRepository.findByNameContainingAndManagerIsNotNull(
             gymName, pageable);
 
-        return climbingGymList.stream()
+        List<AcceptedClimbingGymSimpleResponse> climbingGymList = climbingGymSlice.stream()
             .map(climbingGym -> AcceptedClimbingGymSimpleResponse.toDTO(climbingGym))
             .toList();
 
+        return new PageResponseDto<>(pageable.getPageNumber(), climbingGymSlice.hasNext(),
+            climbingGymList);
 
     }
 }
