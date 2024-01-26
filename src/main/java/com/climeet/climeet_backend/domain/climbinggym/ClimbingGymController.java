8878c7bd9f17802cd7a33bd02855dcc90c3091b6
Ga Dong Sik <62535229+Nesquitto@users.noreply.github.com>
2024-01-26 16:37:55 +0900

    [SPR-66] 암장 조회 기능 개선
    * [SPR-66] feat: search Response 추가
    
    Accepted된 암장 검색에서는 follower도 필요하여 추가
    
    * [SPR-66] feat: Pageable로 20개 불러오기 제한 추가
    
    * [SPR-66] feat: Pagenation 구현
    
    암장 검색에서 page, size로 검색 가능하게 구현
    
    * [SPR-66] feat: Dto에서 비즈니스 로직 제외

--- src/main/java/com/climeet/climeet_backend/domain/climbinggym/ClimbingGymController.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbinggym/ClimbingGymController.java
@@ -1,14 +1,14 @@
 package com.climeet.climeet_backend.domain.climbinggym;
 
+import com.climeet.climeet_backend.domain.climbinggym.dto.ClimbingGymResponseDto.AcceptedClimbingGymSimpleResponse;
 import com.climeet.climeet_backend.domain.climbinggym.dto.ClimbingGymResponseDto.ClimbingGymSimpleResponse;
-import com.climeet.climeet_backend.global.response.ApiResponse;
+import com.climeet.climeet_backend.global.common.PageResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,27 +21,20 @@ public class ClimbingGymController {
 
     private final ClimbingGymService climbingGymService;
 
-    /**
-     * [GET] 암장검색 (Manager가 등록이 되어있지 않은 암장도 조회)
-     * Manager 유무도 함께 반환
-     */
     @Operation(summary = "전국 암장 검색 기능(자체 목록화)")
     @GetMapping("/search/all")
-    public ResponseEntity<List<ClimbingGymSimpleResponse>> getClimbingGymSearchingList(
-        @RequestParam("gymname") String gymName
+    public ResponseEntity<PageResponseDto<List<ClimbingGymSimpleResponse>>> getClimbingGymSearchingList(
+        @RequestParam("gymname") String gymName, @RequestParam int page, @RequestParam int size
     ) {
-        return ResponseEntity.ok(climbingGymService.searchClimbingGym(gymName));
+        return ResponseEntity.ok(climbingGymService.searchClimbingGym(gymName, page, size));
     }
 
-    /**
-     * [GET] 암장검색 (Manager가 등록되어있는 암장만 조회)
-     */
     @Operation(summary = "Manager가 등록된 암장 검색 기능")
     @GetMapping("/search")
-    public ResponseEntity<List<ClimbingGymSimpleResponse>> getAcceptedClimbingGymSearchingList(
-        @RequestParam("gymname") String gymName
+    public ResponseEntity<PageResponseDto<List<AcceptedClimbingGymSimpleResponse>>> getAcceptedClimbingGymSearchingList(
+        @RequestParam("gymname") String gymName, @RequestParam int page, @RequestParam int size
     ) {
-        return ResponseEntity.ok(climbingGymService.searchAcceptedClimbingGym(gymName));
+        return ResponseEntity.ok(climbingGymService.searchAcceptedClimbingGym(gymName, page, size));
     }
 
 }
--- src/main/java/com/climeet/climeet_backend/domain/climbinggym/ClimbingGymService.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbinggym/ClimbingGymService.java
@@ -1,13 +1,13 @@
 package com.climeet.climeet_backend.domain.climbinggym;
 
+import com.climeet.climeet_backend.domain.climbinggym.dto.ClimbingGymResponseDto.AcceptedClimbingGymSimpleResponse;
 import com.climeet.climeet_backend.domain.climbinggym.dto.ClimbingGymResponseDto.ClimbingGymSimpleResponse;
-import com.climeet.climeet_backend.domain.shorts.dto.ShortsResponseDto.ShortsSimpleInfo;
 import com.climeet.climeet_backend.global.common.PageResponseDto;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -16,21 +16,52 @@ public class ClimbingGymService {
 
     private final ClimbingGymRepository climbingGymRepository;
 
-    public List<ClimbingGymSimpleResponse> searchClimbingGym(String gymName) {
-        List<ClimbingGym> climbingGymList = climbingGymRepository.findByNameContaining(gymName);
+    public PageResponseDto<List<ClimbingGymSimpleResponse>> searchClimbingGym(String gymName,
+        int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Slice<ClimbingGym> climbingGymSlice = climbingGymRepository.findByNameContaining(gymName,
+            pageable);
 
-        return climbingGymList.stream()
+        List<ClimbingGymSimpleResponse> climbingGymList = climbingGymSlice.stream()
             .map(climbingGym -> ClimbingGymSimpleResponse.toDTO(climbingGym))
             .toList();
+
+        return new PageResponseDto<>(pageable.getPageNumber(), climbingGymSlice.hasNext(),
+            climbingGymList);
 
     }
 
-    public List<ClimbingGymSimpleResponse> searchAcceptedClimbingGym(String gymName) {
-        List<ClimbingGym> climbingGymList = climbingGymRepository.findByNameContainingAndManagerIsNotNull(
-            gymName);
+    public PageResponseDto<List<AcceptedClimbingGymSimpleResponse>> searchAcceptedClimbingGym(
+        String gymName, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Slice<ClimbingGym> climbingGymSlice = climbingGymRepository.findByNameContainingAndManagerIsNotNull(
+            gymName, pageable);
 
-        return climbingGymList.stream()
-            .map(climbingGym -> ClimbingGymSimpleResponse.toDTO(climbingGym))
+        List<AcceptedClimbingGymSimpleResponse> climbingGymList = climbingGymSlice.stream()
+            .map(climbingGym -> {
+                Long managerId = null;
+                Long follower = 0L;
+                String profileImageUrl = null;
+                // manager 유무 확인
+                if (climbingGym.getManager() != null) {
+                    managerId = climbingGym.getManager().getId();
+                    // manager가 있으면 follower 조회
+                    if (climbingGym.getManager().getFollowerCount() != null) {
+                        follower = climbingGym.getManager().getFollowerCount();
+                    }
+                }
+                // 프로필이 있으면 조회
+                if (climbingGym.getProfileImageUrl() != null) {
+                    profileImageUrl = climbingGym.getProfileImageUrl();
+                }
+
+                return AcceptedClimbingGymSimpleResponse.toDTO(climbingGym, managerId, follower,
+                    profileImageUrl);
+            })
             .toList();
+
+        return new PageResponseDto<>(pageable.getPageNumber(), climbingGymSlice.hasNext(),
+            climbingGymList);
+
     }
 }
