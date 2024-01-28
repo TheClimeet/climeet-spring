89c0a0a5240d6224c532629942dcfe6b289044da
gourderased <gourderased@gmail.com>
2024-01-28 16:59:15 +0900

    [SPR-103] CORS 설정

--- src/main/java/com/climeet/climeet_backend/global/security/WebConfig.java
+++ src/main/java/com/climeet/climeet_backend/global/security/WebConfig.java
@@ -2,23 +2,30 @@ package com.climeet.climeet_backend.global.security;
 
 import com.climeet.climeet_backend.domain.user.UserRepository;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public WebConfig(UserRepository userRepository, JwtTokenProvider jwtTokenProvider) {
-        this.userRepository = userRepository;
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
-
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new CurrentUserArgumentResolver(jwtTokenProvider, userRepository));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("https://climeet.retool.com")
+            .allowedMethods("GET", "POST", "DELETE", "PATCH")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
