a07cb201d52228ba694ebdfc37db9ae2f361b941
gourderased <gourderased@gmail.com>
2024-01-28 17:55:24 +0900

    [Hotfix] 리툴 CORS 에러 해결

--- src/main/java/com/climeet/climeet_backend/global/security/WebConfig.java
+++ src/main/java/com/climeet/climeet_backend/global/security/WebConfig.java
@@ -2,33 +2,30 @@ package com.climeet.climeet_backend.global.security;
 
 import com.climeet.climeet_backend.domain.user.UserRepository;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
-
-    public WebConfig(UserRepository userRepository, JwtTokenProvider jwtTokenProvider) {
-        this.userRepository = userRepository;
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new CurrentUserArgumentResolver(jwtTokenProvider, userRepository));
     }
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//            .allowedOrigins("*") // 허용할 출처
-//            .allowedHeaders("*")
-//            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
-//            .exposedHeaders("Authorization");
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*") // 허용할 출처
+            .allowedHeaders("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+            .exposedHeaders("Authorization");
+    }
 }
