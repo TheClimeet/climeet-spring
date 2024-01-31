ac01ee1f183c8fc1ebb3d0abda31eb66c7bfa7ac
yerim216 <bing_01@naver.com>
2024-01-31 16:13:43 +0900

    [SPR-106] fix: 주석 제거

--- src/main/java/com/climeet/climeet_backend/domain/user/UserService.java
+++ src/main/java/com/climeet/climeet_backend/domain/user/UserService.java
@@ -12,21 +12,27 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class UserService {
+
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final ClimberRepository climberRepository;
-    public User updateNotification(User user, boolean isAllowFollowNotification, boolean isAllowLikeNotification, boolean isAllowCommentNotification, boolean isAllowAdNotification){
-        if(user==null){
+
+    public User updateNotification(User user, boolean isAllowFollowNotification,
+        boolean isAllowLikeNotification, boolean isAllowCommentNotification,
+        boolean isAllowAdNotification) {
+        if (user == null) {
             throw new GeneralException(ErrorStatus._EMPTY_MEMBER);
         }
-        user.updateNotification(isAllowFollowNotification, isAllowLikeNotification, isAllowCommentNotification, isAllowAdNotification);
+        user.updateNotification(isAllowFollowNotification, isAllowLikeNotification,
+            isAllowCommentNotification, isAllowAdNotification);
         return userRepository.save(user);
 
     }
-    public UserTokenSimpleInfo updateUserToken(String refreshToken){
+
+    public UserTokenSimpleInfo updateUserToken(String refreshToken) {
         Long userId = Long.valueOf(jwtTokenProvider.getPayload(refreshToken));
         User user = userRepository.findById(userId)
-                .orElseThrow(()-> new GeneralException(ErrorStatus._INVALID_JWT));
+            .orElseThrow(() -> new GeneralException(ErrorStatus._INVALID_JWT));
         String newRefreshToken = jwtTokenProvider.createRefreshToken(userId);
         String newAccesstoken = jwtTokenProvider.refreshAccessToken(refreshToken);
         user.updateToken(newRefreshToken, newAccesstoken);
@@ -34,5 +40,4 @@ public class UserService {
         return new UserTokenSimpleInfo(user);
 
     }
-//eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxNCIsImlhdCI6MTcwNjY4MDgzMiwiZXhwIjoxNzA2NjgxNDM3fQ.1HFtfxeqiLTinrt0SSrQJaPJt5N5_H8mPwTgxwG4wY8ItrnkaMifOrjz5znqGar1b-jNzXI5sKqp-V0hCcG5KQ
 }
