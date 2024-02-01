fe5e618ce44f3fe8a4bf5c89c37f93702222d8c5
yerim216 <bing_01@naver.com>
2024-02-01 17:50:55 +0900

    [SPR-86] feat: ServiceBitmask enum 추가, swagger enum 명세 추가

---
+++ src/main/java/com/climeet/climeet_backend/domain/climbinggym/enums/ServiceBitmask.java
@@ -0,0 +1,13 @@
+package com.climeet.climeet_backend.domain.climbinggym.enums;
+
+public enum ServiceBitmask {
+        샤워_시설,
+        샤워_용품,
+        수건_제공,
+        간이_세면대,
+        초크_대여,
+        암벽화_대여,
+        삼각대_대여,
+        운동복_대여;
+
+}
