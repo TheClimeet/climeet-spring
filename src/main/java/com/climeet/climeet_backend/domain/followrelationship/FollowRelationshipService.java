dd481bae4763c69237cc8e90138872dae0c0e711
yerim216 <bing_01@naver.com>
2024-02-02 16:39:42 +0900

    [SPR-118] feat: 팔로우 카운팅 증가 함수 이름 변경

--- src/main/java/com/climeet/climeet_backend/domain/followrelationship/FollowRelationshipService.java
+++ src/main/java/com/climeet/climeet_backend/domain/followrelationship/FollowRelationshipService.java
@@ -1,7 +1,6 @@
 package com.climeet.climeet_backend.domain.followrelationship;
 
-import com.climeet.climeet_backend.domain.climber.Climber;
-import com.climeet.climeet_backend.domain.manager.Manager;
+
 import com.climeet.climeet_backend.domain.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
