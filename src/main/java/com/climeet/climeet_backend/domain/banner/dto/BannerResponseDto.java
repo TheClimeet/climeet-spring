2ce098e91c9651cbf0717c3b14fbe062448b89f4
gourderased <gourderased@gmail.com>
2024-02-05 17:29:47 +0900

    [SPR-142] 배너 리스트 조회

--- src/main/java/com/climeet/climeet_backend/domain/banner/dto/BannerResponseDto.java
+++ src/main/java/com/climeet/climeet_backend/domain/banner/dto/BannerResponseDto.java
@@ -1,21 +1,20 @@
 package com.climeet.climeet_backend.domain.banner.dto;
 
 import com.climeet.climeet_backend.domain.banner.Banner;
-import com.climeet.climeet_backend.domain.bestclimber.clear.BestClearClimber;
-import com.climeet.climeet_backend.domain.bestclimber.clear.dto.BestClearClimberResponseDto;
 import java.time.LocalDate;
-import java.util.PrimitiveIterator;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 public class BannerResponseDto {
+
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
-    public static class BannerDetailInfo{
+    public static class BannerDetailInfo {
 
         private Long id;
         private String bannerImageUrl;
@@ -28,7 +27,7 @@ public class BannerResponseDto {
 
 
         public static BannerDetailInfo toDTO(
-            Banner banner){
+            Banner banner) {
 
             return BannerDetailInfo.builder()
                 .id(banner.getId())
@@ -39,6 +38,35 @@ public class BannerResponseDto {
                 .bannerEndDate(banner.getBannerEndDate())
                 .isPopup(banner.getIsPopup())
                 .linkUrl(banner.getLinkUrl())
+                .build();
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class BannerSimpleInfo {
+
+        private Long id;
+        private String title;
+        private LocalDate bannerStartDate;
+        private LocalDate bannerEndDate;
+        private Boolean isPopup;
+        private LocalDateTime createdAt;
+        private String status;
+
+        public static BannerSimpleInfo toDTO(
+            Banner banner, String status) {
+
+            return BannerSimpleInfo.builder()
+                .id(banner.getId())
+                .title(banner.getTitle())
+                .bannerStartDate(banner.getBannerStartDate())
+                .bannerEndDate(banner.getBannerEndDate())
+                .isPopup(banner.getIsPopup())
+                .createdAt(banner.getCreatedAt())
+                .status(status)
                 .build();
         }
     }
