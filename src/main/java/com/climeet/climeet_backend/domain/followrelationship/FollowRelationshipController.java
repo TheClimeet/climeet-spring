2e16d5766b813d9b9b672b89aeeca47eb0ac01fc
yerim216 <bing_01@naver.com>
2024-02-05 21:58:32 +0900

    [SPR-118] refactor: 사용하지 않는 import 지움

--- src/main/java/com/climeet/climeet_backend/domain/followrelationship/FollowRelationshipController.java
+++ src/main/java/com/climeet/climeet_backend/domain/followrelationship/FollowRelationshipController.java
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FollowRelationshipController {
     private final FollowRelationshipService followRelationshipService;
-    private final UserRepository userRepository;
-    private final FollowRelationshipRepository followRelationshipRepository;
 
     @PostMapping("/follow-relationship/{userId}")
     @Operation(summary = "유저 팔로우")
