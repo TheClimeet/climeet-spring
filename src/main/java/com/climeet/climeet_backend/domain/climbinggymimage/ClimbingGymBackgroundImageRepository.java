8a83b97ca3e982ef73a842cb07ec842e5332500a
Ga Dong Sik <62535229+Nesquitto@users.noreply.github.com>
2024-02-08 15:38:50 +0900

    [SPR-137] 암장 기능 관련 수정 및 추가
    * [SPR-137] feat: 암장 배경 이미지 반환 변경
    
    * [SPR-137] feat: 암장 탭 정보 가져오기 기능 추가
    
    * [SPR-137] feat: BackgroundImage 1개로 가정하고 구현 수정

--- src/main/java/com/climeet/climeet_backend/domain/climbinggymimage/ClimbingGymBackgroundImageRepository.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbinggymimage/ClimbingGymBackgroundImageRepository.java
@@ -1,9 +1,12 @@
 package com.climeet.climeet_backend.domain.climbinggymimage;
 
+import com.climeet.climeet_backend.domain.climbinggym.ClimbingGym;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClimbingGymBackgroundImageRepository extends JpaRepository<ClimbingGymBackgroundImage, Long>{
-
+    Optional<ClimbingGymBackgroundImage> findByClimbingGym(ClimbingGym climbingGym);
 }
