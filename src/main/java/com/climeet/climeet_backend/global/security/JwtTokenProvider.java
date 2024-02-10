7e334d1c3be91f3800ebb32d79010ca65cd5b235
yerim216 <bing_01@naver.com>
2024-02-10 15:59:36 +0900

    [SPR-149] feat: refresh token 로직 수정

--- src/main/java/com/climeet/climeet_backend/global/security/JwtTokenProvider.java
+++ src/main/java/com/climeet/climeet_backend/global/security/JwtTokenProvider.java
@@ -3,6 +3,9 @@ package com.climeet.climeet_backend.global.security;
 
 import com.climeet.climeet_backend.domain.climber.Climber;
 import com.climeet.climeet_backend.domain.climber.ClimberRepository;
+import com.climeet.climeet_backend.domain.manager.Manager;
+import com.climeet.climeet_backend.domain.user.User;
+import com.climeet.climeet_backend.domain.user.UserRepository;
 import com.climeet.climeet_backend.global.response.code.BaseErrorCode;
 import com.climeet.climeet_backend.global.response.code.status.ErrorStatus;
 import com.climeet.climeet_backend.global.response.exception.GeneralException;
@@ -29,7 +32,9 @@ import org.springframework.stereotype.Service;
 @Configuration
 @RequiredArgsConstructor
 @PropertySource("classpath:application-dev.yml")
+@Service
 public class JwtTokenProvider {
+
     @Value("${jwt.access-token.expire-length}")
     private long accessTokenValidityInMilliseconds;
     @Value("${jwt.refresh-token.expire-length}")
@@ -37,8 +42,9 @@ public class JwtTokenProvider {
     @Value("${jwt.custom.secretKey}")
     private String SECRET_KEY;
 
+
     private SecretKey cachedSecretKey;
-    private final ClimberRepository climberRepository;
+    private final UserRepository userRepository;
 
     private SecretKey _getSecretKey() {
         String keyBase64Encoded = Base64.getEncoder().encodeToString(SECRET_KEY.getBytes());
@@ -48,13 +54,15 @@ public class JwtTokenProvider {
 
     // 시크릿 키를 반환하는 method
     public SecretKey getSecretKey() {
-        if (cachedSecretKey == null) cachedSecretKey = _getSecretKey();
+        if (cachedSecretKey == null)
+            cachedSecretKey = _getSecretKey();
 
         return cachedSecretKey;
     }
-  public String createAccessToken(String payload){
-      return createToken(payload, accessTokenValidityInMilliseconds);
-  }
+
+    public String createAccessToken(String payload) {
+        return createToken(payload, accessTokenValidityInMilliseconds);
+    }
 
     public String createRefreshToken(Long climberId) {
         return createToken(climberId.toString(), refreshTokenValidityInMillseconds);
@@ -62,9 +70,20 @@ public class JwtTokenProvider {
 
     public String refreshAccessToken(String refreshToken) {
         if (validateToken(refreshToken)) {
-            Long climberId = Long.parseLong(getPayload(refreshToken));
-            Climber climber = climberRepository.findById(climberId).orElseThrow(() -> new GeneralException(ErrorStatus._INVALID_MEMBER));
-            return createAccessToken(climber.getPayload());
+            Long userId = Long.parseLong(getPayload(refreshToken));
+            User user = userRepository.findById(userId)
+                .orElseThrow(() -> new GeneralException(ErrorStatus._INVALID_MEMBER));
+            String payload = null;
+            if (user instanceof Climber) {
+                Climber climber = (Climber) user;
+                payload = climber.getPayload();
+            }
+            if (user instanceof Manager) {
+                Manager manager = (Manager) user;
+                payload = manager.getPayload();
+            }
+            return createAccessToken(payload);
+
         } else {
             throw new GeneralException(ErrorStatus._INVALID_JWT);
         }
