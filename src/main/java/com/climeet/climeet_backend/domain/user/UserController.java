488638c81af27a3eeb922f74d2de755913a27fbc
yerim216 <bing_01@naver.com>
2024-02-11 17:31:30 +0900

    [SPR-122] fix: controller 변수 선언부 제거

--- src/main/java/com/climeet/climeet_backend/domain/user/UserController.java
+++ src/main/java/com/climeet/climeet_backend/domain/user/UserController.java
@@ -31,8 +31,7 @@ public class UserController {
     @Operation(summary = "소셜 Access token, Refresh token 재발급 ")
     @SwaggerApiError({ErrorStatus._INVALID_JWT, ErrorStatus._EXPIRED_JWT, ErrorStatus._INVALID_MEMBER})
     public ResponseEntity<UserTokenSimpleInfo> refreshToken(@RequestParam String refreshToken){
-        UserTokenSimpleInfo userTokenSimpleInfo = userService.updateUserToken(refreshToken);
-        return ResponseEntity.ok(userTokenSimpleInfo);
+        return ResponseEntity.ok(userService.updateUserToken(refreshToken));
 
 
     }
@@ -40,8 +39,7 @@ public class UserController {
     @Operation(summary = "특정 유저 팔로워 조회", description = "**userCategory** : Manager OR Climber")
     @SwaggerApiError({ErrorStatus._BAD_REQUEST, ErrorStatus._EMPTY_USER})
     public ResponseEntity<List<UserFollowDetailInfo>> getFollower(@RequestParam Long userId, @RequestParam String userCategory, @CurrentUser User currentUser){
-        List<UserFollowDetailInfo> userFollowDetailResponseList = userService.getFollower(userId, currentUser, userCategory);
-        return ResponseEntity.ok(userFollowDetailResponseList);
+        return ResponseEntity.ok(userService.getFollower(userId, currentUser, userCategory));
 
     }
 
@@ -49,8 +47,7 @@ public class UserController {
     @Operation(summary = "특정 유저 팔로잉 조회", description = "**userCategory** : Manager OR Climber")
     @SwaggerApiError({ErrorStatus._BAD_REQUEST, ErrorStatus._EMPTY_USER})
     public ResponseEntity<List<UserFollowDetailInfo>> getFollowing(@RequestParam Long userId, @RequestParam String userCategory, @CurrentUser User currentUser){
-        List<UserFollowDetailInfo> userFollowDetailResponseList = userService.getFollowing(userId, currentUser, userCategory);
-        return ResponseEntity.ok(userFollowDetailResponseList);
+        return ResponseEntity.ok(userService.getFollowing(userId, currentUser, userCategory));
 
     }
 
