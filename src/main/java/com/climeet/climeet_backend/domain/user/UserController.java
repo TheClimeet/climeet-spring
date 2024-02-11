13386ee62f9457e278274b111a8a359f4a41251e
yerim216 <bing_01@naver.com>
2024-02-11 16:07:18 +0900

    [SPR-115] refactor: 사용하지 않는 Import문 제거

--- src/main/java/com/climeet/climeet_backend/domain/user/UserController.java
+++ src/main/java/com/climeet/climeet_backend/domain/user/UserController.java
@@ -4,19 +4,14 @@ import com.climeet.climeet_backend.domain.user.dto.UserResponseDto.UserFollowDet
 import com.climeet.climeet_backend.domain.user.dto.UserResponseDto.UserTokenSimpleInfo;
 import com.climeet.climeet_backend.global.response.code.status.ErrorStatus;
 import com.climeet.climeet_backend.global.security.CurrentUser;
-import com.climeet.climeet_backend.global.security.JwtTokenProvider;
 import com.climeet.climeet_backend.global.utils.SwaggerApiError;
-import io.jsonwebtoken.ExpiredJwtException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
