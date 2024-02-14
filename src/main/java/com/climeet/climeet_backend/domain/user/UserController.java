6b1442e1603d61aaa7c932d4e061303adda35723
yerim216 <bing_01@naver.com>
2024-02-14 20:35:18 +0900

    [SPR-123] Refactor: requestMapping /api 추가

--- src/main/java/com/climeet/climeet_backend/domain/user/UserController.java
+++ src/main/java/com/climeet/climeet_backend/domain/user/UserController.java
@@ -20,12 +20,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "User")
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
