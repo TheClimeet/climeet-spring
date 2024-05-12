5e89d933b760f9d7842ce521cb332d5a17b41332
Ga Dong Sik <ret0422@gmail.com>
2024-05-12 23:31:42 +0900

    [SPR-184] feat: climb_data json column 추가
    RouteVersion 테이블에 climb_data JSON컬럼을 추가함
    추후 모든 변경 이후에 Route, Sector List 컬럼을 삭제할 예정

--- src/main/java/com/climeet/climeet_backend/domain/routeversion/RouteVersion.java
+++ src/main/java/com/climeet/climeet_backend/domain/routeversion/RouteVersion.java
@@ -1,8 +1,10 @@
 package com.climeet.climeet_backend.domain.routeversion;
 
+import io.hypersistence.utils.hibernate.type.json.JsonType;
 import com.climeet.climeet_backend.domain.climbinggym.ClimbingGym;
 import com.climeet.climeet_backend.domain.climbinggymlayoutimage.ClimbingGymLayoutImage;
 import com.climeet.climeet_backend.global.utils.BaseTimeEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -10,12 +12,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull.List;
 import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 @Builder
 @Getter
@@ -42,6 +46,10 @@ public class RouteVersion extends BaseTimeEntity {
 
     @NotNull
     private String sectorList;
+
+    @Type()
+    @Column(columnDefinition = "json")
+    private Map<String, List<Long>> climbData;
 
 
     public static RouteVersion toEntity(ClimbingGym climbingGym, LocalDate timePoint,
