43f8bc0c64ce6c5354f508d9766302d01ad8dbd5
Ga Dong Sik <62535229+Nesquitto@users.noreply.github.com>
2024-05-16 10:30:57 +0900

    [SPR-184] RouteVersion 테이블 구조 수정
    * [SPR-184] feat: climb_data json column 추가
    
    RouteVersion 테이블에 climb_data JSON컬럼을 추가함
    추후 모든 변경 이후에 Route, Sector List 컬럼을 삭제할 예정
    
    * [SPR-184] feat: climb_data json column 추가
    
    빠뜨린 부분 수정
    
    * [SPR-184] refac: 쿼리 JSON 형태에 맞춰서 리팩토링
    
    리팩토링 하는김에 RouteVersion 관련 쿼리 최적화도 함께 처리
    
    * [SPR-184] refac: 쿼리 JSON 형태에 맞춰서 리팩토링
    
    JSON으로 변경 완료
    
    * [SPR-184] refac: 쿼리 JSON 형태에 맞춰서 변경
    
    route_list, sector_list는 이제 필요없도록 세팅 완료
    
    * [SPR-184] refac: Converter 파일 삭제 및 불필요 주석 삭제
    
    더이상 사용하지 않음
    
    * [SPR-184] refac: 불필요 dependency 제거
    
    JSON 인식하게 하려고 2개의 의존성 라이브러리 사용했는데, 실제로는 1개로도 동작하여 다시 제거

--- src/main/java/com/climeet/climeet_backend/domain/routeversion/RouteVersionRepository.java
+++ src/main/java/com/climeet/climeet_backend/domain/routeversion/RouteVersionRepository.java
@@ -2,16 +2,32 @@ package com.climeet.climeet_backend.domain.routeversion;
 
 import com.climeet.climeet_backend.domain.climbinggym.ClimbingGym;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RouteVersionRepository extends JpaRepository<RouteVersion, Long> {
-    List<RouteVersion> findByClimbingGymOrderByTimePointDesc(ClimbingGym climbingGym);
+
+    @Query("SELECT rv.timePoint "
+        + "FROM RouteVersion rv "
+        + "WHERE rv.climbingGym.id = :gymId "
+        + "ORDER BY rv.timePoint desc")
+    List<LocalDate> findTimePointListByGymId(@Param("gymId") Long gymId);
 
     Optional<RouteVersion> findFirstByClimbingGymAndTimePointLessThanEqualOrderByTimePointDesc(ClimbingGym climbingGym, LocalDate timePoint);
+
+    @Query("SELECT rv.climbData "
+        + "FROM RouteVersion rv "
+        + "WHERE rv.climbingGym = :climbingGym "
+        + "AND rv.timePoint <= :timePoint "
+        + "ORDER BY rv.timePoint "
+        + "LIMIT 1")
+    Map<String, List<Long>> findClimbDataByClimbingGymAndTimePoint(@Param("climbingGym") ClimbingGym climbingGym, @Param("timePoint") LocalDate timePoint);
 
     Optional<RouteVersion> findByClimbingGymAndTimePoint(ClimbingGym climbingGym, LocalDate timePoint);
 }
