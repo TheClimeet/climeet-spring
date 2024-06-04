d6de209ab6e0c91c760a80939ae9698e86bd5361
tjdgns8439 <100510247+tjdgns8439@users.noreply.github.com>
2024-06-04 23:01:08 +0900

    [HOTFIX] fix: 쿼리 실행 에러 수정

--- src/main/java/com/climeet/climeet_backend/domain/climbinggym/ClimbingGymRepository.java
+++ src/main/java/com/climeet/climeet_backend/domain/climbinggym/ClimbingGymRepository.java
@@ -1,10 +1,13 @@
 package com.climeet.climeet_backend.domain.climbinggym;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,5 +17,6 @@ public interface ClimbingGymRepository extends JpaRepository<ClimbingGym, Long> 
 
     Page<ClimbingGym> findByNameContaining(String gymName, Pageable pageable);
 
-    Page<ClimbingGym> findByNameContainingAndManagerIsNotNull(String gymName, Pageable pageable);
+    @Query("SELECT cg FROM ClimbingGym cg LEFT JOIN Manager m on cg.id = m.climbingGym.id WHERE cg.name LIKE %:name% AND m.id IS NOT NULL")
+    Page<ClimbingGym> findByNameContainingAndManagerIsNotNull(@Param("name") String name, Pageable pageable);
 }
