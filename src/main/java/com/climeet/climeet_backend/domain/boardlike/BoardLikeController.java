c8cf37e1b28c6a18cb4c8ced4a6f194d1ee40f1b
yerim216 <bing_01@naver.com>
2024-06-23 16:59:42 +0900

    [SPR-195] docs: SwaggerError 추가

--- src/main/java/com/climeet/climeet_backend/domain/boardlike/BoardLikeController.java
+++ src/main/java/com/climeet/climeet_backend/domain/boardlike/BoardLikeController.java
@@ -1,7 +1,9 @@
 package com.climeet.climeet_backend.domain.boardlike;
 
 import com.climeet.climeet_backend.domain.user.User;
+import com.climeet.climeet_backend.global.response.code.status.ErrorStatus;
 import com.climeet.climeet_backend.global.security.CurrentUser;
+import com.climeet.climeet_backend.global.utils.SwaggerApiError;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ public class BoardLikeController {
     private final BoardLikeService boardLikeService;
 
     @PatchMapping("/boards/{boardId}/like")
+    @SwaggerApiError({ErrorStatus._BOARD_NOT_FOUND, ErrorStatus._EXIST_BOARD_LIKE})
     @Operation(summary = "특정 공지사항 좋아요")
     public ResponseEntity<String> likeNotice(@CurrentUser User user, @PathVariable Long boardId){
         boardLikeService.boardLike(user, boardId);
@@ -22,6 +25,7 @@ public class BoardLikeController {
     }
 
     @PatchMapping("/boards/{boardId}/unlike")
+    @SwaggerApiError({ErrorStatus._BOARD_NOT_FOUND, ErrorStatus._UNEXIST_BOARD_LIKE})
     @Operation(summary = "특정 공지사항 좋아요 취소")
     public ResponseEntity<String> unLikeNotice(@CurrentUser User user, @PathVariable Long boardId){
         boardLikeService.boardUnLike(user, boardId);
