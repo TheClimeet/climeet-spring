00ac47a1ae87421e54670858dbb6fc5075d02538
yerim216 <bing_01@naver.com>
2024-07-02 13:33:32 +0900

    Update submodule to the latest main branch

---
+++ src/main/java/com/climeet/climeet_backend/domain/climber/scheduler/ClimberCleanupScheduler.java
@@ -0,0 +1,12 @@
+package com.climeet.climeet_backend.domain.climber.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ClimberCleanupScheduler {
+
+
+
+}
