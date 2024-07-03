3372ce055b09b1824dfbeaba5d69da4f73779583
Ga Dong Sik <ret0422@gmail.com>
2024-07-04 01:09:18 +0900

    [SPR-211] feat: 루트 버전 전체 불러오기 api 엔드포인트 변경

--- src/main/java/com/climeet/climeet_backend/domain/routeversion/RouteVersionController.java
+++ src/main/java/com/climeet/climeet_backend/domain/routeversion/RouteVersionController.java
@@ -77,7 +77,7 @@ public class RouteVersionController {
 
     @Operation(summary = "암장 특정 루트버전의 모든 데이터 불러오기 (루트버전 수정용) - 1105 [무빗]")
     @SwaggerApiError({ErrorStatus._EMPTY_MANAGER, ErrorStatus._EMPTY_VERSION})
-    @GetMapping("/{gymId}/version/all")
+    @GetMapping("/version/all")
     public ResponseEntity<RouteVersionAllDataResponse> getRouteVersionAllData(
         @CurrentUser User user, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate timePoint) {
         return ResponseEntity.ok(
