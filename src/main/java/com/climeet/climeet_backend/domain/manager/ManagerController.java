54cb6b70c34fcfd93753bfeeae861b238452ca14
yerim216 <bing_01@naver.com>
2024-07-12 00:34:04 +0900

    [SPR-218] fix: 테스트용 코드 제거

--- src/main/java/com/climeet/climeet_backend/domain/manager/ManagerController.java
+++ src/main/java/com/climeet/climeet_backend/domain/manager/ManagerController.java
@@ -75,15 +75,4 @@ public class ManagerController {
         return ResponseEntity.ok("관리자 탈퇴 신청 완료. 클밋 승인 후 탈퇴 처리 됩니다. 7일 이내 재로그인 시 정보가 복구됩니다.");
     }
 
-    @PostMapping("/delete")
-    @Operation(summary = "관리자 hard delete test")
-    public ResponseEntity<String> hardDeleteManager(@CurrentUser User user){
-        managerService.deleteManager(user);
-        return ResponseEntity.ok("탈퇴 완료");
-    }
-
-
-
-
-
 }
